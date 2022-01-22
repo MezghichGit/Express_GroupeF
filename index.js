69a6c60fb328d4469ea5c37152e16a8d259ecc29
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-01-22 14:29:44 +0100

    request get

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ app.listen(85,
         console.log("Express Server Started....");
     }
 )
+
+const equipes = require("./equipes.json");
+
+app.get("/equipes",(request, response)=>{
+    response.send("<h1 align=center>Liste des équipes</h1>");
+})
